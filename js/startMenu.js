6a53def97b14ed57c059764df1a00c02ba63adda
JakePhillips-Davies <162450362+JakePhillips-Davies@users.noreply.github.com>
2024-10-20 21:07:57 +0100

    223

--- js/startMenu.js
+++ js/startMenu.js
@@ -1,5 +1,6 @@
 const startButton = document.getElementById('startButton');
 const startMenu = document.getElementById('startMenu');
+const listButtons = document.getElementById('startList').querySelectorAll('button');
 
 function closeStartMenu() {
     startButton.checked = false;
@@ -8,4 +9,10 @@ function closeStartMenu() {
 document.addEventListener('click', function(e) {
     if(!startMenu.contains(e.target) && !startButton.contains(e.target) && !(window.getComputedStyle(startMenu, null).display == 'none'))
         closeStartMenu();
+});
+
+listButtons.forEach(button_ => {
+    button_.addEventListener('click', () => {
+        openWindow(document.getElementById(button_.getAttribute("target")), '_blank');
+    });
 });
